0a40cbbf24d84eb6aa863177e7c7a143a5984564
SidBala <Sid.Bala@outlook.com>
2016-07-30 13:09:52 -0700

    There can be only one commit

---
+++ QuickBuildApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuickBuildApp
+//
+//  Created by Sid Bala on 7/30/16.
+//  Copyright © 2016 Sid Bala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QuickBuildApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QuickBuildApp
+//
+//  Created by Sid Bala on 7/30/16.
+//  Copyright © 2016 Sid Bala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
